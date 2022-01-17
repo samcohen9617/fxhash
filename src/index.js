c18ffde580a582b76f9883844ab0762e29ba739d
sam453 <sam@obsessvr.com>
2022-01-17 00:00:57 -0500

    loaded glb

--- src/index.js
+++ src/index.js
@@ -30,42 +30,144 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 //   random hash: ${fxhash}\n
 //   some pseudo random values: [ ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()},... ]\n
 // `
-const scene = new THREE.Scene();
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial( { color: 0xFFFFFF} );
+// const cube = new THREE.Mesh( geometry, material );
+// const scene = new THREE.Scene();
+
+// class World {
+//   constructor() {
+//     // synchronous setup here
+//     // create camera, renderer, scene, etc.
+//   }
+
+//   async init() {
+//     console.log("PATH", process.env.ASSET_PATH);
+//     const mesh = await loadModels();
+
+//     scene.add( mesh );
+//     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+//     const renderer = new THREE.WebGLRenderer();
+//     renderer.setSize( window.innerWidth, window.innerHeight );
+//     document.body.appendChild( renderer.domElement );
+//     renderer.render( scene, camera );
+//   }
+// }
+
+// async function main() {
+//   const world = new World();
+
+//   // complete async tasks
+//   await world.init();
+// }
+
+// main().catch((err) => {
+//   console.error(err);
+// });
 
 
-class World {
-  constructor() {
-    // synchronous setup here
-    // create camera, renderer, scene, etc.
-  }
+// async function loadModels() {
+//   const loader = new GLTFLoader();
 
-  async init() {
-    console.log("PATH", process.env.ASSET_PATH);
-    await loadModels();
+//   const modelData = await loader.loadAsync("./Parrot.glb");
+//   const model = modelData.scene.children[0];
 
-  }
+
+//   console.log('Squaaawk!', modelData);
+//   return model;
+
+// }
+
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
+camera.position.z = 80;
+camera.position.y = 10;
+
+var renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setClearColor("#e5e5e5");
+renderer.setSize(window.innerWidth,window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+})
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshLambertMaterial({color: 0xF7F7F7});
+//var mesh = new THREE.Mesh(geometry, material);
+
+//scene.add(mesh);
+
+let meshX = -10;
+for(var i = 0; i<15;i++) {
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = (Math.random() - 0.5) * 10;
+    mesh.position.y = (Math.random() - 0.5) * 10;
+    mesh.position.z = (Math.random() - 0.5) * 10;
+    scene.add(mesh);
+    meshX+=1;
 }
 
-async function main() {
-  // Get a reference to the container element
-  const container = document.querySelector('#scene-container');
 
-  // create a new world
-  const world = new World(container);
+var light = new THREE.PointLight(0xFFFFFF, 1, 1000)
+light.position.set(0,0,0);
+scene.add(light);
 
-  // complete async tasks
-  await world.init();
-}
+var light = new THREE.PointLight(0xFFFFFF, 2, 1000)
+light.position.set(0,0,25);
+scene.add(light);
 
-main().catch((err) => {
-  console.error(err);
-});
-
-
-async function loadModels() {
+var loadModel = async function(){
   const loader = new GLTFLoader();
 
   const modelData = await loader.loadAsync("./Parrot.glb");
+  const model = modelData.scene.children[0];
+
 
   console.log('Squaaawk!', modelData);
+  scene.add(model);
 }
+
+const loadAsync = async () =>{
+  await loadModel();
+}
+loadAsync();
+
+var render = function() {
+    requestAnimationFrame(render);
+
+    
+    renderer.render(scene, camera);
+}
+
+function onMouseMove(event) {
+    event.preventDefault();
+
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    var intersects = raycaster.intersectObjects(scene.children, true);
+    for (var i = 0; i < intersects.length; i++) {
+        this.tl = new TimelineMax();
+        this.tl.to(intersects[i].object.scale, 1, {x: 2, ease: Expo.easeOut})
+        this.tl.to(intersects[i].object.scale, .5, {x: .5, ease: Expo.easeOut})
+        this.tl.to(intersects[i].object.position, .5, {x: 2, ease: Expo.easeOut})
+        this.tl.to(intersects[i].object.rotation, .5, {y: Math.PI*.5, ease: Expo.easeOut}, "=-1.5")
+    }
+}
+
+
+
+window.addEventListener('mousemove', onMouseMove);
+render();
